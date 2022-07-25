f38cd5d860197f424879d1b4370520272053a540
Juan Jáuregui <juan@wavesinaroom.com>
2022-07-25 15:56:48 -0500

    [Feature] Create player factor that includes player token enum

--- game.js
+++ game.js
@@ -1,3 +1,13 @@
 function GameBoard (){
   const tiles = [9];
+  return tiles;
+}
+
+enum PlayerToken{
+  naughts,
+  crosses,
+}
+
+function Player(name:string, token:PlayerToken, score:int){
+  return name, token, score;
 }
