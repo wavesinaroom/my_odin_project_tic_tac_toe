37bd00f8166e199f80bbda9016e53c2eec0464da
Juan Jáuregui <juan@wavesinaroom.com>
2022-07-25 17:40:19 -0500

    [Feature] Add player prototype plus player token and redo Gamboard function

--- game.js
+++ game.js
@@ -1,13 +1,29 @@
-function GameBoard (){
-  const tiles = [9];
-  return tiles;
-}
+const PlayerToken={
+  even: Symbol("even"),
+  odd: Symbol("odd"),
+  none: Symbol("none")
+};
 
-enum PlayerToken{
-  even,
-  odd,
-}
-
-function Player(name:string, token:PlayerToken, score:int){
+function Player(name, token, score){
   return name, token, score;
+}
+
+function Tile(value, player){
+  return value, player;
+}
+
+function GameBoard (){
+
+  const gameBoardSize = 3;
+  let tiles = [[gameBoardSize],[gameBoardSize],[gameBoardSize]];
+
+  for(let i = 0; i < gameBoardSize; ++i)
+  {
+    for(let k = 0; k < gameBoardSize; ++k)
+    {
+      this.tiles[i][k] = new Tile(0,none);
+      console.log(this.tiles[i][k]);
+    }
+  }
+  return tiles;
 }
