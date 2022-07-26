94d9f558b70f5710f4a466384e72ae5b8afa2c9b
Juan Jáuregui <juan@wavesinaroom.com>
2022-07-26 15:19:21 -0500

    [Task] Outline Game Manager for later prototype functions

--- game.js
+++ game.js
@@ -17,15 +17,33 @@ const GameBoard = (()=>{
   const gameBoardSize = 3;
   let tiles = [[gameBoardSize],[gameBoardSize],[gameBoardSize]];
 
+})();
+
+GameBoard.prototype.SetsBoard = function () {
   for(let i = 0; i < gameBoardSize; ++i)
   {
     for(let k = 0; k < gameBoardSize; ++k)
     {
-      tiles[i][k] = Tile(0,PlayerToken.none);
-      console.log(tiles[i][k]);
+      this.tiles[i][k] = Tile(0,PlayerToken.none);
     }
   }
-  return tiles;
-})();
+  return this.tiles;
+};
+
 
 //Game Manager
+const GameManager = (()=>{
+  //Starts game
+  //Sets up game mode P vs P or P vs CPU
+  //Sets up players
+  //Manages turns
+  //Assigns scores
+  //Announces winner
+  //Resets game
+})();
+
+GameManager.prototype.StartGame = function(){
+
+}
+
+GameManager.SetupGameMode
