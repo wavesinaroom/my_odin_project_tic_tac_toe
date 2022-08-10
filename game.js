49172b6d559bab4f2e62e713feb2bb7e22de3ce9
Juan Jáuregui <juan@wavesinaroom.com>
2022-08-10 18:01:29 -0500

    [Feature] Delete board and players area on end game

--- game.js
+++ game.js
@@ -51,6 +51,7 @@ const GameManager = (()=>{
 
   let playerInTurn = players[0];
 
+
   const ManageGameTurn = (board) => {
 
     let sum = 0;
@@ -68,13 +69,13 @@ const GameManager = (()=>{
       {
         if(GameManager.playerInTurn.name == GameManager.players[0].name)
         {
-          //winner function players[0]
+          GameBoard.HideBoard();
           alert('P1 Winner');
           return;
         }
         else
         {
-          //winner function players[1]
+          GameBoard.HideBoard();
           alert('P2 Winner');
           return;
         }
@@ -95,13 +96,13 @@ const GameManager = (()=>{
       {
           if(GameManager.playerInTurn.name == GameManager.players[0].name)
           {
-            //winner function players[0]
+            GameBoard.HideBoard();
             alert('P1 Winner');
             return;
           }
           else
           {
-            //winner function players[1]
+            GameBoard.HideBoard();
             alert('P2 Winner');
             return;
           }
@@ -121,14 +122,12 @@ const GameManager = (()=>{
     {
         if(GameManager.playerInTurn.name == GameManager.players[0].name)
         {
-          //winner function players[0]
-          alert('P1 Winner');
+          GameBoard.HideBoard();
           return;
         }
         else
         {
-          //winner function players[1]
-          alert('P2 Winner');
+          GameBoard.HideBoard();
           return;
         }
     }
@@ -147,14 +146,12 @@ const GameManager = (()=>{
     {
         if(GameManager.playerInTurn.name == GameManager.players[0].name)
         {
-          //winner function players[0]
-          alert('P1 Winner');
+          GameBoard.HideBoard();
           return;
         }
         else
         {
-          //winner function players[1]
-          alert('P2 Winner');
+          GameBoard.HideBoard();
           return;
         }
     }
@@ -174,7 +171,7 @@ const GameManager = (()=>{
 
     if(tileCount==Math.pow(board.length, 2))
     {
-      alert('tie');
+      GameBoard.HideBoard();
       return;
     }
 
@@ -188,108 +185,119 @@ const GameManager = (()=>{
       throw "invalid value for player turn";
     }
   }
-  return /*StartGame, ResetGame, SetUpGameMode, SetUpPlayers,*/ {players,playerInTurn, ManageGameTurn};
+
+  return /*SetUpGame*/ {players,playerInTurn, ManageGameTurn};
 
 })();
 
 const GameBoard = (()=>{
 
-  const gameBoardSize = 3;
-
-  let board = [[gameBoardSize],[gameBoardSize],[gameBoardSize]];
   let boardDiv = document.createElement('div');
   boardDiv.className = "boardDiv";
   documentBody.appendChild(boardDiv);
 
-  let onClickNumber;
-  let numbersOnBoard=[];
-  //Tiles
-  for(let row = 0; row < gameBoardSize; ++row)
-  {
-    let boardRow = document.createElement('td');
-    boardRow.className = "boardRow";
-    for(let col = 0; col < gameBoardSize; ++col)
+  let playersAreaDiv = document.createElement('div');
+  playersAreaDiv.className = "playersArea";
+  documentBody.appendChild(playersAreaDiv);
+
+  const SetUpBoard = () =>{
+    const gameBoardSize = 3;
+    let board = [[gameBoardSize],[gameBoardSize],[gameBoardSize]];
+
+    let onClickNumber;
+    let numbersOnBoard=[];
+    //Tiles
+    for(let row = 0; row < gameBoardSize; ++row)
     {
-      board[row][col] = Tile(0, PlayerToken.none, document.createElement('td'));
-      board[row][col].htmlTile.className = "boardTile";
-      board[row][col].htmlTile.textContent = "Yo";
-      board[row][col].htmlTile.addEventListener('click', ()=>{
+      let boardRow = document.createElement('td');
+      boardRow.className = "boardRow";
+      for(let col = 0; col < gameBoardSize; ++col)
+      {
+        board[row][col] = Tile(0, PlayerToken.none, document.createElement('td'));
+        board[row][col].htmlTile.className = "boardTile";
+        board[row][col].htmlTile.textContent = "Yo";
+        board[row][col].htmlTile.addEventListener('click', ()=>{
 
-        if(board[row][col].tileToken!=undefined)
-          throw 'Tile\'s already been taken';
+          if(board[row][col].tileToken!=undefined)
+            throw 'Tile\'s already been taken';
 
-        for(let numberPos = 0; numberPos<numbersOnBoard.length; ++numberPos)
-        {
-          if(onClickNumber==numbersOnBoard[numberPos])
+          for(let numberPos = 0; numberPos<numbersOnBoard.length; ++numberPos)
           {
-            alert("Number\'s already on the board");
+            if(onClickNumber==numbersOnBoard[numberPos])
+            {
+              alert("Number\'s already on the board");
+              return;
+            }
+          }
+
+          if(onClickNumber!=undefined)
+          {
+            board[row][col].htmlTile.textContent = onClickNumber;
+            board[row][col].tileValue = onClickNumber;
+            board[row][col].tileToken = GameManager.playerInTurn.token;
+            GameManager.ManageGameTurn(board);
+            turnDisplayHTML.textContent = GameManager.playerInTurn.name;
+            numbersOnBoard.push(onClickNumber);
+          }
+        });
+        boardRow.appendChild(board[row][col].htmlTile);
+      }
+      boardDiv.appendChild(boardRow);
+    }
+
+    //Turn Display
+    let turnDisplayHTML = document.createElement('p');
+    turnDisplayHTML.className = "turnDisplay";
+    turnDisplayHTML.textContent = GameManager.playerInTurn.name;
+    documentBody.appendChild(playersAreaDiv);
+
+    //Players
+
+    GameManager.players.forEach(player  => {
+      let playerHTML = document.createElement('div');
+      playerHTML.className = "player"
+      playersAreaDiv.appendChild(playerHTML);
+
+      let playerNameHTML = document.createElement('h1');
+      playerNameHTML.textContent = player.name;
+      playerHTML.appendChild(playerNameHTML);
+
+      let playerTokenHTML = document.createElement('p');
+      if(player.token==PlayerToken.even)
+      {
+        playerTokenHTML.textContent = "Even";
+      }else{
+        playerTokenHTML.textContent = "Odd";
+      }
+      playerHTML.appendChild(playerTokenHTML);
+
+      let playerNumbersArrayHTML = document.createElement('div');
+      playerNumbersArrayHTML.className = "playerNumbers";
+      playerHTML.appendChild(playerNumbersArrayHTML);
+
+      for(let i = 0; i<player.numbers.length; ++i)
+      {
+        let playerNumberHTML = document.createElement('p');
+        playerNumberHTML.textContent = player.numbers[i];
+        playerNumberHTML.addEventListener("click", () =>{
+          if(player!=GameManager.playerInTurn)
+          {
+            alert('Not your turn yet');
+            onClickNumber = undefined;
             return;
           }
-        }
-
-        if(onClickNumber!=undefined)
-        {
-          board[row][col].htmlTile.textContent = onClickNumber;
-          board[row][col].tileValue = onClickNumber;
-          board[row][col].tileToken = GameManager.playerInTurn.token;
-          GameManager.ManageGameTurn(board);
-          turnDisplayHTML.textContent = GameManager.playerInTurn.name;
-          numbersOnBoard.push(onClickNumber);
-        }
-      });
-      boardRow.appendChild(board[row][col].htmlTile);
-    }
-    boardDiv.appendChild(boardRow);
+          onClickNumber = player.numbers[i];
+        });
+        playerNumbersArrayHTML.appendChild(playerNumberHTML);
+      }
+    });
   }
 
-  //Turn Display
-  let turnDisplayHTML = document.createElement('p');
-  turnDisplayHTML.className = "turnDisplay";
-  turnDisplayHTML.textContent = GameManager.playerInTurn.name;
-  documentBody.appendChild(turnDisplayHTML);
-
-  //Players
-  let playersAreaHTML = document.createElement('div');
-  playersAreaHTML.className = "playersArea";
-  documentBody.appendChild(playersAreaHTML);
-
-  GameManager.players.forEach(player  => {
-    let playerHTML = document.createElement('div');
-    playerHTML.className = "player"
-    playersAreaHTML.appendChild(playerHTML);
-
-    let playerNameHTML = document.createElement('h1');
-    playerNameHTML.textContent = player.name;
-    playerHTML.appendChild(playerNameHTML);
-
-    let playerTokenHTML = document.createElement('p');
-    if(player.token==PlayerToken.even)
-    {
-      playerTokenHTML.textContent = "Even";
-    }else{
-      playerTokenHTML.textContent = "Odd";
-    }
-    playerHTML.appendChild(playerTokenHTML);
-
-    let playerNumbersArrayHTML = document.createElement('div');
-    playerNumbersArrayHTML.className = "playerNumbers";
-    playerHTML.appendChild(playerNumbersArrayHTML);
-
-    for(let i = 0; i<player.numbers.length; ++i)
-    {
-      let playerNumberHTML = document.createElement('p');
-      playerNumberHTML.textContent = player.numbers[i];
-      playerNumberHTML.addEventListener("click", () =>{
-        if(player!=GameManager.playerInTurn)
-        {
-          alert('Not your turn yet');
-          onClickNumber = undefined;
-          return;
-        }
-        onClickNumber = player.numbers[i];
-      });
-      playerNumbersArrayHTML.appendChild(playerNumberHTML);
-    }
-
-  });
+  const HideBoard = () =>{
+    documentBody.removeChild(boardDiv);
+    documentBody.removeChild(playersAreaDiv);
+  }
+  return {SetUpBoard, HideBoard};
 })();
+
+GameBoard.SetUpBoard();
