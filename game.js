1ae1070fb3681ccca39a4cf331f25e82d18c7adc
wavesinaroom <juan@wavesinaroom.com>
2022-09-06 09:44:27 -0500

    [Task] Board cell final adjustments iteration three

--- game.js
+++ game.js
@@ -249,7 +249,7 @@ const GameBoard = (()=>{
           {
             board[row][col].htmlTile.textContent = onClickNumber;
             board[row][col].htmlTile.style.color = '#E81A55';
-            board[row][col].htmlTile.style.backgroundColor = '#E8D91A';
+            board[row][col].htmlTile.style.backgroundColor = '#EBE189';
             board[row][col].htmlTile.style.backdropFilter = 'opacity(20%)';
             board[row][col].tileValue = onClickNumber;
             board[row][col].tileToken = GameManager.playerInTurn.token;
